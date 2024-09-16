2e0bd500fe4db92c0e63d605d008af37aea10a36
ALNoman78 <abdullaalno078@gmail.com>
2024-09-16 16:39:17 +0600

    practice

--- js/github.js
+++ js/github.js
@@ -1,29 +1,29 @@
-// step - 1
-document.getElementById('text').addEventListener('keyup', function(event){
-    const typingElement = event.target.value;
-    const enableBtn = document.getElementById('delete-element')
-    if (typingElement === 'delete') {
-        enableBtn.removeAttribute('disabled')
-    }else{
-        enableBtn.setAttribute('disabled', true)
-    }
-})
+// // step - 1
+// document.getElementById('text').addEventListener('keyup', function(event){
+//     const typingElement = event.target.value;
+//     const enableBtn = document.getElementById('delete-element')
+//     if (typingElement === 'delete') {
+//         enableBtn.removeAttribute('disabled')
+//     }else{
+//         enableBtn.setAttribute('disabled', true)
+//     }
+// })
 
-// step - 2
-document.getElementById('delete-element').addEventListener('click', function(){
-    const removeElement = document.getElementById('heading-element');
-    removeElement.style.display = "none";
-})
+// // step - 2
+// document.getElementById('delete-element').addEventListener('click', function(){
+//     const removeElement = document.getElementById('heading-element');
+//     removeElement.style.display = "none";
+// })
 
 
-// section -02 
+// // section -02 
 
-document.getElementById('change-element').addEventListener('click', function(){
-    const inputElement = document.getElementById ('new-text');
-    const textElement = inputElement.value
-    console.log(textElement);
+// document.getElementById('change-element').addEventListener('click', function(){
+//     const inputElement = document.getElementById ('new-text');
+//     const textElement = inputElement.value
+//     console.log(textElement);
 
-    const changeHeaingElement = document.getElementById('cng-heading');
-    changeHeaingElement.innerText = textElement;
-    inputElement.value = "";
-})
+//     const changeHeaingElement = document.getElementById('cng-heading');
+//     changeHeaingElement.innerText = textElement;
+//     inputElement.value = "";
+// })
--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
-document.getElementById('btn-comment').addEventListener('click', function(){
-    const inputField = document.getElementById('input-field');
-    const postedComment = inputField.value
-    const newComment = document.createElement('p')
-    newComment.innerText = postedComment;
-    // step - 2
-    const commentPost = document.getElementById('comment-post');
-    commentPost.appendChild(newComment)
-    // again input-field empty
-    inputField.value = '';
+// document.getElementById('btn-comment').addEventListener('click', function(){
+//     const inputField = document.getElementById('input-field');
+//     const postedComment = inputField.value
+//     const newComment = document.createElement('p')
+//     newComment.innerText = postedComment;
+//     // step - 2
+//     const commentPost = document.getElementById('comment-post');
+//     commentPost.appendChild(newComment)
+//     // again input-field empty
+//     inputField.value = '';
 
+// })
+
+
+
+
+
+
+
+
+
+// task -2
+
+    // step - 01
+document.getElementById('btn-comment').addEventListener('click', function(){
+    const inputField = document.getElementById('input-field')
+    const inputValue = inputField.value;
+    
+    // step - 02
+
+    const newComponent = document.createElement('p')
+    newComponent.innerText = inputValue;
+    console.log(newComponent);
+
+    // step - 03
+
+    const commentSection = document.getElementById('comment-post');
+    commentSection.appendChild(newComponent);
+
+    inputField.value = "";
 })
